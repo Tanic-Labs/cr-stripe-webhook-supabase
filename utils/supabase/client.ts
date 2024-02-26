cde3a06c2693fe9c753166f4ab46cc1cdeb50eb9
Sentpiqe <sentpiqe@proton.me>
2024-02-26 13:53:00 -0600

    fix deploy

--- utils/supabase/client.ts
+++ utils/supabase/client.ts
@@ -1,6 +1,6 @@
 import * as supabase from "https://esm.sh/@supabase/supabase-js@2";
 
 export const supabaseClient = supabase.createClient(
-  Deno.env.get("SUPABASE_URL"),
-  Deno.env.get("SUPABASE_TOKEN")
+  Deno.env.get("SUPABASE_URL") || '',
+  Deno.env.get("SUPABASE_TOKEN") || '',
 );
