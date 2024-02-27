31924575785ca8abfbaaf243ce5c482fe1ea3b41
Sentpiqe <sentpiqe@proton.me>
2024-02-27 08:11:01 -0600

    update env

--- utils/stripe/config.ts
+++ utils/stripe/config.ts
@@ -1,4 +1,4 @@
 import Stripe from "npm:stripe@^13.0.0";
-export const stripe = new Stripe(Deno.env.get('STRIPE_PUBLIC_KEY') || '', {
+export const stripe = new Stripe(Deno.env.get('STRIPE_SECRET_KEY') || '', {
     apiVersion: "2023-10-16",
 });
